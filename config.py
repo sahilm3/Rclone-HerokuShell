327dab236acf006c9193a4a077610ff88e4fd3e7
sahilm3 <90495590+sahilm3@users.noreply.github.com>
2021-12-09 12:50:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 w = open('config.json', 'w+')
 w.write('{')
 w.write('\n')
-w.write('    "authToken": "5047072747:AAFIy0TETwvgKInDkA4bT4opG1V0w8GXkeY",')
+w.write('    "authToken": "'5047072747:AAFIy0TETwvgKInDkA4bT4opG1V0w8GXkeY'",')
 w.write('\n')
 w.write('    "owner": '830785064'))
 w.write('\n')
